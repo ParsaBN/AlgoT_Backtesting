e4fdf9171169b35f9bf3eb59886b7fec790370b4
Liam Brennan <liam@anywhich.com>
2023-10-16 19:35:38 +1100

    i have to go

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # region imports
+from datetime import datetime
 from AlgorithmImports import *
 from QuantConnect.Data.UniverseSelection import *
 from dataclasses import dataclass
+
+import QuantConnect.Data.UniverseSelection
 # endregion
 
 # @dataclass
@@ -31,6 +34,27 @@ from dataclasses import dataclass
 #     def score(self) -> float:
 #         return self.price_earnings_ratio
 
+class SymbolData(object):
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.fast_average = SimpleMovingAverage(50)
+        self.slow_average = SimpleMovingAverage(200)
+        self.rsi = RelativeStrengthIndex(14)
+        # self.is_uptrend = False
+
+    def is_ready(self) -> bool:
+        return self.fast_average.IsReady and self.slow_average.IsReady and self.rsi.IsReady
+
+    def update(self, time: datetime, value: float):
+        # if self.fast_average.Update(time, value) and self.slow_average.Update(time, value):
+        #     fast = self.fast_average.Current.Value
+        #     slow = self.slow_average.Current.Value
+        #     self.is_uptrend = fast > slow
+
+        self.fast_average.Update(time, value)
+        self.slow_average.Update(time, value)
+        self.rsi.Update(time, value)
+
 class CryingRedRhinoceros(QCAlgorithm):
     def Initialize(self):
         # self.stock_metrics: Dict[Symbol, StockMetrics] = {}
@@ -38,20 +62,22 @@ class CryingRedRhinoceros(QCAlgorithm):
         self.SetStartDate(2014, 1, 1)
         self.SetEndDate(2015, 1, 1)
         self.SetCash(50000)
-        self.SetWarmUp(200, Resolution.Daily)
+        # self.SetWarmUp(200, Resolution.Daily)
 
-        # what resolution should the data *added* to the universe be?
+        # self.AddEquity("AAPL")
+
+        self.symbol_data: Dict[Symbol, SymbolData] = {}
+        self.selected_securities: List[Security] = []
+
         self.UniverseSettings.Resolution = Resolution.Daily
-
-        # this add universe method accepts two parameters:
-        # - coarse selection function: accepts an IEnumerable<CoarseFundamental> and returns an IEnumerable<Symbol>
-        # - fine selection function: accepts an IEnumerable<FineFundamental> and returns an IEnumerable<Symbol>
         self.AddUniverse(self.CoarseSelectionFunction, self.FineSelectionFunction)
 
-        self.are_trackers_initialised = False
-        self.short_sma: Dict[Symbol, SimpleMovingAverage] = {}
-        self.long_sma: Dict[Symbol, SimpleMovingAverage] = {}
-        self.rsi: Dict[Symbol, RelativeVigorIndex] = {}
+        self.Schedule.On(self.DateRules.EveryDay(), self.TimeRules.Midnight, self.Rebalance)
+
+        # self.are_trackers_initialised = False
+        # self.short_sma: Dict[Symbol, SimpleMovingAverage] = {}
+        # self.long_sma: Dict[Symbol, SimpleMovingAverage] = {}
+        # self.rsi: Dict[Symbol, RelativeStrengthIndex] = {}
 
     def CoarseSelectionFunction(self, coarse: List[CoarseFundamental]) -> List[Symbol]:
         filtered = [x for x in coarse if x.HasFundamentalData][:10]
@@ -66,56 +92,62 @@ class CryingRedRhinoceros(QCAlgorithm):
         #     self.AddEquity(i.Symbol, Resolution.Daily)
 
         # return [i.Symbol for i in top]
-
             # self.AddSecurity
 
-        for x in filtered:
-            if not x.Symbol in self.ActiveSecurities:
-                self.AddSecurity(x.Symbol, Resolution.Daily)
+        # for x in filtered:
+        #     if not x.Symbol in self.ActiveSecurities:
+        #         self.AddSecurity(x.Symbol, Resolution.Daily) # TODO: finer resolution for stop loss?
 
-                self.short_sma[x.Symbol] = self.SMA(x.Symbol, 50, Resolution.Daily)
-                self.long_sma[x.Symbol] = self.SMA(x.Symbol, 200, Resolution.Daily)
-                self.rsi[x.Symbol] = self.RSI(x.Symbol, 14, MovingAverageType.Simple, Resolution.Daily)
+        #         self.short_sma[x.Symbol] = self.SMA(x.Symbol, 50, Resolution.Daily)
+        #         self.long_sma[x.Symbol] = self.SMA(x.Symbol, 200, Resolution.Daily)
+        #         self.rsi[x.Symbol] = self.RSI(x.Symbol, 14, MovingAverageType.Simple, Resolution.Daily)
 
-            # self.short_sma = { x: self.SMA(x.Symbol, 50, Resolution.Daily) for x in filtered }
-            # self.long_sma = { x: self.SMA(x.Symbol, 200, Resolution.Daily) for x in filtered }
-            # self.rsi = { x: self.RSI(x.Symbol, 14, MovingAverageType.Simple, Resolution.Daily) for x in filtered }
+        #     # self.short_sma = { x: self.SMA(x.Symbol, 50, Resolution.Daily) for x in filtered }
+        #     # self.long_sma = { x: self.SMA(x.Symbol, 200, Resolution.Daily) for x in filtered }
+        #     # self.rsi = { x: self.RSI(x.Symbol, 14, MovingAverageType.Simple, Resolution.Daily) for x in filtered }
 
-            # self.are_trackers_initialised = True
+        #     # self.are_trackers_initialised = True
+
+        for cf in filtered:
+            if cf.Symbol not in self.symbol_data:
+                self.symbol_data[cf.Symbol] = SymbolData(cf.Symbol)
+
+            data = self.symbol_data[cf.Symbol]
+            data.update(cf.EndTime, cf.AdjustedPrice)
 
         return [x.Symbol for x in filtered]
 
     def FineSelectionFunction(self, fine: List[FineFundamental]) -> List[Symbol]:
-        # stock_metrics = { x.Symbol: StockMetrics(x.ValuationRatios.PERatio, x.AssetClassification.GrowthScore, x.FinancialStatements.CashFlowStatement, x.OperationRatios.TotalDebtEquityRatio.OneMonth) for x in fine }
-
-        # growth_scores = { x.Symbol: StockMetrics(x.AssetClassification.GrowthScore) for x in fine }
-        # cashflow_statements = { x.Symbol: StockMetrics(x.FinancialStatements.CashFlowStatement) for x in fine }
-
-        # DE_ratio = { x.Symbol: StockMetrics(x.OperationRatios.TotalDebtEquityRatio.OneMonth) for x in fine }
-
-        # free_cashflow = { x.Symbol: StockMetrics(x.FinancialStatements.CashFlowStatement.FreeCashFlow.OneMonth) for x in fine }
+        # selected = [x.Symbol for x in fine if
+        #     x.ValuationRatios.PERatio < 15 and
+        #     x.AssetClassification.GrowthScore > 0.15 and
         
-        # # a = { x.Symbol: StockMetrics(x.FinancialStatements.CashFlowStatement.CashFlow.OneMonth) for x in fine }
+        #     self.short_sma[x.Symbol].IsReady and self.long_sma[x.Symbol].IsReady and
+        #     self.short_sma[x.Symbol].Current.Value > self.long_sma[x].Current.Value and 
 
-        selected = [x.Symbol for x in fine if
-            x.ValuationRatios.PERatio < 15 and
-            x.AssetClassification.GrowthScore > 0.15 and
-        
-            self.short_sma[x.Symbol].IsReady and
-            self.long_sma[x.Symbol].IsReady and
-            self.short_sma[x.Symbol].Current.Value > self.long_sma[x].Current.Value and 
+        #     self.rsi[x.Symbol].IsReady and
+        #     self.rsi[x.Symbol].Current.Value > 0
+        # ]
 
-            self.rsi[x.Symbol].IsReady and
-            self.rsi[x.Symbol].Current.Value > 0
-        ]
+        # if len(selected) > 0:
+        #     self.Log(len(selected))
+        #     self.Log(selected[0])
 
-        if len(selected) > 0:
-            self.Log(len(selected))
-            self.Log(selected[0])
+        # return selected
+
+        selected = []
+        for ff in fine:
+            data = self.symbol_data[ff.Symbol]
+            should_select = (data.is_ready() and
+                ff.ValuationRatios.PERatio < 20 and
+                ff.AssetClassification.GrowthScore > 0.075 and
+                data.fast_average.Current.Value > data.slow_average.Current.Value and
+                data.rsi.Current.Value > 0)
+
+            if should_select:
+                selected.append(ff.Symbol)
 
         return selected
-
-
 
 
         # filtered_fine = [x for x in fine if x.ValuationRatios.PERatio 
@@ -170,10 +202,43 @@ class CryingRedRhinoceros(QCAlgorithm):
         
         # return [x.Symbol for x in self.chosen]
 
+    def Rebalance(self):
+        if self.Time.month != 1 and self.Time.month != 7:
+            return
+        
+        self.Log('rebalancing!')
+
+        self.Log('self.Portfolio')
+        for s in self.Portfolio.Keys:
+            self.Log(s)
+
+        self.Log('self.Securities')
+        for s in self.Securities.Keys:
+            self.Log(s)
+
+        self.Log('self.ActiveSecurities')
+        for s in self.ActiveSecurities:
+            self.Log(s)
+
+        # for symbol in self.ActiveSecurities:
+        #     self.Liquidate(symbol)
+        #     self.Log(f'was active: {symbol}')
+
+        # if len(self.UniverseSelection) > 0:
+        #     percentage = 1 / len(self.Securities)
+        #     for symbol in self.Securities:
+        #         self.SetHoldings(symbol, percentage)
+
+        #     self.Log('current securities:')
+        #     for s in self.Securities:
+        #         self.Log(f'{s}: {percentage}')
+
 
     def AllocatePortfolio(self, selection: List[str]): # TODO: is selection: List[], also is selection ordered? no
-        # first, allocate 10% to the top 10
         # second, assume selection is ordered? or like [(a, score_a), (b, score_b)]
+
+        # max allocation to one stock is 5-10%
+        # keep a dictionary of sectors and ensure total sector 
 
         # liquidate everything
         self.Liquidate()
@@ -181,15 +246,22 @@ class CryingRedRhinoceros(QCAlgorithm):
         for stock in selection[:10]:
             self.SetHoldings(stock, 0.1)
 
-
-
     def ScoreStock(self, stock) -> float:
         # given a stock return score between 0 and 1
         pass
 
+    def OnSecuritiesChanged(self, changes: SecurityChanges):
+        for security in changes.RemovedSecurities:
+            self.selected_securities.remove(security)
+
+        for security in changes.AddedSecurities:
+            self.SetHoldings(security.Symbol, 0.01)
+            self.selected_securities.append(security)
 
     def OnData(self, data: Slice):
-        if not self.Portfolio.Invested:
-            self.SetHoldings("SPY", 0.33)
-            self.SetHoldings("BND", 0.33)
-            self.SetHoldings("AAPL", 0.33)
+        # if not self.Portfolio.Invested:
+        #     # self.SetHoldings("SPY", 0.33)
+        #     # self.SetHoldings("BND", 0.33)
+        #     self.SetHoldings("AAPL", 0.33)
+        
+        pass
