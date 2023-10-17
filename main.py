15ec6abc04c8c587fa1f295c7fa0302b8a717c2b
Liam Brennan <liam@anywhich.com>
2023-10-18 00:14:31 +1100

    ok pretty good

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ class SymbolData(object):
         self.fast_average = SimpleMovingAverage(50)
         self.slow_average = SimpleMovingAverage(200)
         self.rsi = RelativeStrengthIndex(14)
-        # self.is_uptrend = False
 
     def is_ready(self) -> bool:
         return self.fast_average.IsReady and self.slow_average.IsReady and self.rsi.IsReady
@@ -26,12 +25,30 @@ class Metric:
     get: Callable[[FineFundamental, SymbolData], float]
     min: float
     max: float
+    importance: float
     reversed: bool = False
+    hard_min: bool = True
 
 METRICS = {
-    'price_earnings_ratio': Metric(lambda f, d: f.ValuationRatios.PERatio, 7.5, 20, reversed=True),
-    'growth': Metric(lambda f, d: f.AssetClassification.GrowthScore, 0.02, 1),
-    'fast_slow_average_crossover': Metric(lambda f, d: (d.fast_average.Current.Value - d.slow_average.Current.Value) / d.slow_average.Current.Value, 0, 0.2),
+    'price_earnings_ratio': Metric(
+        lambda f, d: f.ValuationRatios.PERatio,
+        min=7.5, max=20.0,
+        importance=1.5,
+        reversed=True
+    ),
+
+    'growth': Metric(
+        lambda f, d: f.AssetClassification.GrowthScore,
+        min=0.02, max=1.0,
+        importance=1.0
+    ),
+
+    'fast_slow_average_crossover': Metric(
+        lambda f, d: (d.fast_average.Current.Value - d.slow_average.Current.Value) / d.slow_average.Current.Value,
+        min=0.0, max=0.2,
+        importance=1.0
+    ),
+    
     # 'relative_strength_index': Metric(lambda f, d: d.rsi.Current.Value, 7.5, 20),
 }
 
@@ -42,19 +59,19 @@ class CryingRedRhinoceros(QCAlgorithm):
         self.SetCash(100000)
         self.SetWarmUp(1, Resolution.Daily)
 
+        # something is kinda broken
         # see: https://www.quantconnect.com/forum/discussion/8779/security-doesn-039-t-have-a-bar-of-data-trade-error-options-trading/p1
-        # something is still broken
         # self.SetSecurityInitializer(lambda x: x.SetMarketPrice(self.GetLastKnownPrice(x)))
 
         self.symbol_data: Dict[Symbol, SymbolData] = {}
         self.selected_scores: Dict[Security, float] = {}
 
         self.UniverseSettings.Resolution = Resolution.Daily
-        self.AddUniverse(self.CoarseSelectionFunction, self.FineSelectionFunction)
+        self.AddUniverse(self.CoarseSelection, self.FineSelection)
 
         self.Schedule.On(self.DateRules.MonthStart(0), self.TimeRules.Midnight, self.Rebalance)
 
-    def CoarseSelectionFunction(self, coarse: List[CoarseFundamental]) -> List[Symbol]:
+    def CoarseSelection(self, coarse: List[CoarseFundamental]) -> List[Symbol]:
         filtered = [x for x in coarse if x.HasFundamentalData][:100]
 
         for cf in filtered:
@@ -66,12 +83,12 @@ class CryingRedRhinoceros(QCAlgorithm):
 
         return [x.Symbol for x in filtered]
 
-    def FineSelectionFunction(self, fine: List[FineFundamental]) -> List[Symbol]:
+    def FineSelection(self, fine: List[FineFundamental]) -> List[Symbol]:
         self.selected_scores = {}
         for ff in fine:
             ff.Price
             data = self.symbol_data[ff.Symbol]
-            score = self.ScoreStock(ff, data)
+            score = self.CalculateScore(ff, data)
 
             if score is not None:
                 self.selected_scores[ff.Symbol] = score
@@ -87,7 +104,6 @@ class CryingRedRhinoceros(QCAlgorithm):
         for security in self.Portfolio.Values:
             if security.Invested:
                 self.Liquidate(security.Symbol)
-                # self.Log(f'liquidated: {symbol}')
 
         if self.selected_scores:
             total_score = sum(self.selected_scores.values())
@@ -96,20 +112,21 @@ class CryingRedRhinoceros(QCAlgorithm):
                 self.SetHoldings(symbol, score / total_score)
                 self.Log(f'set holding for {symbol}: {score / total_score} (score: {score})')
 
-    def ScoreStock(self, fine: FineFundamental, data: SymbolData) -> Union[float, None]:
-        scores = {}
-        for name, metric in METRICS.items():
+    def CalculateScore(self, fine: FineFundamental, data: SymbolData) -> Union[float, None]:
+        score = 0
+        for metric in METRICS.values():
             raw = (metric.get(fine, data) - metric.min) / (metric.max - metric.min)
 
             if metric.reversed:
                 raw = 1 - raw
 
-            if raw < 0:
+            if metric.hard_min and raw < 0:
                 return None
             
-            scores[name] = min(raw, 1.0) # already clamped > 0
+            clamped = min(raw, 1.0) # already clamped > 0
+            score += clamped * metric.importance
 
-        return sum(scores.values()) # FIXME: needs parameterised importance scaling
+        return score # should we normalise 0 to 1?
 
     # def OnSecuritiesChanged(self, changes: SecurityChanges):
     #     pass
